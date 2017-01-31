1de1325b81ab20b789e4b6237188449b861855ec
Chris Sherman <chris@cloudistics.com>
2017-01-31 15:57:12 -0500

    Scroll to the top of the view on route change.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'D.C. Volunteer';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // Scroll to the top of the view when the route changes.
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      document.body.scrollTop = 0;
+    });
+  }
 }
